920807e80aed3ed63bc2fda638f84489af16eb01
Altas dallas <atlas.webdev89@gmail.com>
2022-07-19 15:03:36 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,46 +1,93 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+    const exprArr = expr.split("");
+
+    // Разделяем массив по блокам 10 знаков.
+    const data = [];
+    for (let i = 0; i < exprArr.length; i += 10) {
+        data.push(exprArr.slice(i, i + 10));
+    }
+
+    // Убираем начальные нули
+    const dataCode = data.map((value) => {
+        for (let j = 0; j < value.length; j++) {
+            if (value[j] === "1") {
+                value.splice(0, j);
+                return value;
+            } else if (value[j] === "*") {
+                return value;
+            }
+        }
+    });
+
+    // Преобразуем в точки-тире
+    const Morse = dataCode.map((value) => {
+        let strMorse = "";
+        for (let j = 0; j < value.length; j += 2) {
+            if (value[j] === "*") {
+                strMorse += "space";
+                break;
+            }
+            if (value[j] + value[j + 1] === "10") {
+                strMorse += ".";
+            } else if (value[j] + value[j + 1] === "11") {
+                strMorse += "-";
+            }
+        }
+        return strMorse;
+    });
+
+    // Преобразуем в сообщение
+    const message = Morse.reduce((string, element) => {
+        if (element === "space") {
+            string += " ";
+        } else {
+            string += MORSE_TABLE[element];
+        }
+        return string;
+    }, "");
+
+    return message;
 }
 
 module.exports = {
-    decode
-}
+    decode,
+};
